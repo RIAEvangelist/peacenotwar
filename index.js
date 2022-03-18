600995649349ef301dd56cf4eea3b80611975d80
ryanolee <rizza@rizza.net>
2022-03-18 01:58:57 +0000

    Add back in hook and remove intrusiveness of message

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import fs, { readFileSync } from 'fs';
 
-var thinkaboutit='WITH-LOVE-FROM-THE-WORLD.txt';
-var WITH_LOVE_FROM_THE_WORLD=fs.readFileSync(`./${thinkaboutit}`);
-
 // Send a message of love without war.
 // Without the need of taking the war like path.
 // An eye for an eye make the world blind.
 // Diplomacy wins out over action,
 // which is a lot more of what the world needs at this stage. 
 // <3
-console.log(WITH_LOVE_FROM_THE_WORLD.toString())
 
 var whatWeWant='♥';
 
